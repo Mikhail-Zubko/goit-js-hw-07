dfe9bd0f5626407ad8d4df5fa8a7a8371cb78212
Mikhail <99561817+Mikhail-Zubko@users.noreply.github.com>
2024-06-04 13:49:39 +0300

    task_1_done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,68 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", onShowFullImg);
+
+gallery.insertAdjacentHTML("beforeend", createGalarryMarkup(galleryItems));
+
+function createGalarryMarkup(pictures) {
+  const markup = pictures
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </li>`;
+    })
+    .join("");
+  return markup;
+}
+
+const showFullPic = basicLightbox.create(`<img src="">`, {
+    onShow: instance => {
+        window.addEventListener("keydown", onCloseImg);
+    },
+    onClose: instance => {
+        window.removeEventListener("keydown", onCloseImg);
+    }
+})
+
+console.log(basicLightbox.create(`<img src="">`));
+
+function onShowFullImg(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+    showFullPic.element().querySelector("img").src = e.target.dataset.source;
+    showFullPic.show()
+}
+
+function onCloseImg(e) {
+  if (e.code === "Escape") {
+      showFullPic.close()
+      return
+  }
+}
+/*
+Создание и рендер разметки по массиву данных galleryItems
+ и предоставленному шаблону элемента галереи.
+Реализация делегирования на ul.gallery и получение
+ url большого изображения.
+Подключение скрипта и стилей библиотеки модального окна
+ basicLightbox. Используй CDN сервис jsdelivr и добавь 
+ в проект ссылки на минифицированные (.min) файлы библиотеки.
+Открытие модального окна по клику на элементе галереи.
+ Для этого ознакомься с документацией и примерами.
+Замена значения атрибута src элемента <img> в модальном 
+окне перед открытием. Используй готовую разметку модального
+ окна с изображением из примеров библиотеки basicLightbox.
+*/
